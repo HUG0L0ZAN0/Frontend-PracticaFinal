17ee867867228df2c10b4f148576737271701c80
HUG0L0ZAN0 <A01383795@tec.mx>
2025-05-21 17:11:50 -0600

    homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,24 @@
-// src/components/HomePage.js
 import React from 'react';
+import './HomePage.css';
 
 export default function HomePage() {
   return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h1>Hello World</h1>
+    <div className="home-container">
+      <h1>Hola profe</h1>
+
+      <p>
+        Le voy a listar porque me deberia de calificar como destacado:
+      </p>
+
+      <ul>
+        <li>CRUD.</li>
+        <li>SQL Server: Conexión desde Node.js.</li>
+        <li>Hashing de contraseñas con bcrypt.</li>
+        <li>Autenticación JWT.</li>
+        <li>React &amp; localStorage.</li>
+      </ul>
+
+      
     </div>
   );
 }
