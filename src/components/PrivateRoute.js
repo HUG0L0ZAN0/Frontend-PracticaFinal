c72b30e771c92ab0a3dafaa19a65092ebc0b9c63
HUG0L0ZAN0 <A01383795@tec.mx>
2025-05-21 20:32:58 -0600

    ultimas correcciones

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,6 +3,6 @@ import { Navigate } from 'react-router-dom';
 export default function PrivateRoute({ children }) {
   const token = localStorage.getItem('token');
   return token
-    ? children                       // Si hay token, renderiza lo que envuelva
-    : <Navigate to="/login" replace />;  // Si no, redirige al login
+    ? children
+    : <Navigate to="/login" replace />;
 }
