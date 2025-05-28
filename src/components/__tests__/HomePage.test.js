b4886df9a2bc68fd39a7b4d11cc525f65c3f26f4
HUG0L0ZAN0 <A01383795@tec.mx>
2025-05-27 19:08:15 -0600

    test

--- src/components/__tests__/HomePage.test.js
+++ src/components/__tests__/HomePage.test.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
 import HomePage from '../HomePage';
-
-// Mock the PrivateRoute component to allow rendering without authentication
-jest.mock('../PrivateRoute', () => {
-  return function MockPrivateRoute({ children }) {
-    return children;
-  };
-});
-
-// Mock the ProtectedLayout component
-jest.mock('../ProtectedLayout', () => {
-  return function MockProtectedLayout({ children }) {
-    return children;
-  };
-});
 
 describe('HomePage Component', () => {
   test('renders "Hola profe" text', () => {
-    render(
-      <BrowserRouter>
-        <HomePage />
-      </BrowserRouter>
-    );
-    
+    render(<HomePage />);
     const headingElement = screen.getByText('Hola profe');
     expect(headingElement).toBeInTheDocument();
   });
