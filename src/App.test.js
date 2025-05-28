db79b7abfee0e87a8ae6f6fd2b8ae461961a872f
HUG0L0ZAN0 <A01383795@tec.mx>
2025-05-27 19:24:32 -0600

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+
+// Mock the components that use react-router
+jest.mock('./components/LoginPage', () => () => <div>Login Page</div>);
+jest.mock('./components/RegisterPage', () => () => <div>Register Page</div>);
+jest.mock('./components/HomePage', () => () => <div>Home Page</div>);
+jest.mock('./components/ProfilePage', () => () => <div>Profile Page</div>);
+jest.mock('./components/PrivateRoute', () => ({ children }) => children);
+jest.mock('./components/ProtectedLayout', () => ({ children }) => children);
 
 test('renders without crashing', () => {
   render(
-    <BrowserRouter>
+    <MemoryRouter>
       <App />
-    </BrowserRouter>
+    </MemoryRouter>
   );
 });
