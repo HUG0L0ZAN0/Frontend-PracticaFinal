59a8d9f219a0f792d29fbe52f8493aeff733812f
HUG0L0ZAN0 <A01383795@tec.mx>
2025-05-27 19:20:57 -0600

    test

--- src/components/__tests__/HomePage.test.js
+++ src/components/__tests__/HomePage.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import HomePage from './HomePage';
+import '@testing-library/jest-dom';
+import HomePage from '../HomePage';
 
 test('muestra el saludo Hola profe', () => {
   render(<HomePage />);
